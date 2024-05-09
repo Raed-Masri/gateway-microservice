bfd6ceed3311a9df224758a32738667787e5a6ce
Raed Masri <raedmasri75@gmail.com>
2024-05-09 22:19:19 +0300

    changes

--- config/routes.js
+++ config/routes.js
@@ -2,46 +2,61 @@ export const routes = [
   {
     method: "post",
     port: "3002",
-    path: "/videos/create",  //create video
+    path: "/videos/create", //create video
   },
   {
     method: "get",
     port: "3002",
-    path: "/videos",   //get video
+    path: "/videos", //get video
   },
   {
     method: "get",
     port: "3002",
-    path: "/videos/comments/:id",   //get comments
+    path: "/videos/play", //play video
   },
   {
     method: "get",
     port: "3002",
-    path: "/videos/play",
+    path: "/videos/comments/:id", //get comments
   },
   {
     method: "post",
     port: "3002",
-    path: "/videos/comments/:id",   //add comments
-  },
-  {
-    method: "post",
-    port: "3002",
-    path: "/videos/rating/:id",   //add rating
+    path: "/videos/comments/:id", //add comments
   },
   {
     method: "patch",
     port: "3002",
-    path: "/videos/:id/:commentId",  //update comment
+    path: "/videos/:id/:commentId", //update comment
   },
-  // {
-  //   method: "get",
-  //   port: "3002",
-  //   path: "/videos/comments/:id",
-  // },
-  // {
-  //   method: "get",
-  //   port: "3002",
-  //   path: "/videos/comments/:id",
-  // },
+  {
+    method: "post",
+    port: "3002",
+    path: "/videos/:id/comments/:commentId/reply", //reply to comment comment
+  },
+  {
+    method: "post",
+    port: "3002",
+    path: "/videos/rating/:id", //add rating
+  },
+  {
+    method: "post",
+    port: "3001",
+    path: "/user/sign-in", //sign in
+  },
+  {
+    method: "post",
+    port: "3001",
+    path: "/user/sign-in", //sign up
+  },
+  {
+    method: "patch",
+    port: "3001",
+    path: "/user/update", //update user
+  },
+  {
+    method: "get",
+    port: "3001",
+    path: "/user/:email", //get user
+  },
 ];
